6adcf471882ff99b5b8a6fe556319c16842539a4
Eduardo Briceno <83840504+EduardoBriceno89@users.noreply.github.com>
2023-05-29 13:26:37 -0600

    Login (#1)
    * Add login design
    
    * Added some validations to matricula and password field

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent }, // <-- Aquí se coloca la ruta por defecto
+  { path: 'login', component: LoginComponent }, // <-- Aquí se coloca la ruta login
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
