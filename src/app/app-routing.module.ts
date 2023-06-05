1b109c1c57ec1046e04448d6c97c5b86e5b434ec
EduardoBriceno89 <eduardbrice@outlook.com>
2023-06-05 13:17:51 -0600

    Add admin template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { PanelComponent } from './componentes/panel/panel.component';
+import { AdminDashboardComponent } from './componentes/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent }, // <-- Aquí se coloca la ruta por defecto
   { path: 'login', component: LoginComponent }, // <-- Aquí se coloca la ruta login
   { path: 'panel', component: PanelComponent },
+  { path: 'admin', component: AdminDashboardComponent },
 ];
 
 @NgModule({
