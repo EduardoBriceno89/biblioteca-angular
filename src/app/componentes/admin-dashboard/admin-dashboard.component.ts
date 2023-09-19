64ffc3221dd8314ac581a0d748956cfcbe4d7b19
EduardoBriceno89 <eduardbrice@outlook.com>
2023-09-19 11:29:37 -0600

    Enhanced mobile view

--- src/app/componentes/admin-dashboard/admin-dashboard.component.ts
+++ src/app/componentes/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { NavigationComponent } from './navigation/navigation.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
